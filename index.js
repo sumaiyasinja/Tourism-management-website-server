8dc2dc9cd348c13228460c9633a7db22ecfeee51
Sumaiya Sinja <sumiyasinja@gmail.com>
2025-02-10 00:24:58 +0600

    all of Users API created

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient } = require('mongodb'); 
+const { MongoClient, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 
@@ -22,21 +22,37 @@ async function run() {
     const database = client.db("TravelSpotManagement");
     const userCollection = database.collection("users");
 
-
+    // Users API 
       app.get("/users", async (req, res) => {
         const result = await userCollection.find().toArray();
         res.send(result);
       });
+
       app.get("/users/:id", async (req, res) => {
         const id = req.params.id;
         const query = {
           _id: new ObjectId(id),
         };
-        const result = await usersCollection.findOne(query);
+        const result = await userCollection.findOne(query);
         console.log(result);
         res.send(result);
       });
 
+      app.post("/users", async(req,res)=>{
+        const user = req.body
+        const query = { email : user.email}
+        const existingUser = await userCollection.findOne(query)
+        if(existingUser){
+            res.send({ message: "user already exists", insertedId: null });
+            return
+        }
+        try{
+            const result = await userCollection.insertOne(user);
+            res.send(result)
+        }catch(error){
+            res.status(500).send({ message: "Error inserting user to database", error: error.message });
+        }
+      })
 
 
 
